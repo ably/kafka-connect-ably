a2fec8b8ce57ee3913ff5a0cc6d9679d19270390
Matt Esch <matt@mattesch.info>
2021-03-12 12:39:52 +0000

    add license info

--- src/main/java/io/ably/kakfa/connect/package-info.java
+++ src/main/java/io/ably/kakfa/connect/package-info.java
@@ -1,4 +1,20 @@
 /**
+ * Copyright © 2021 Ably Real-time Ltd. (support@ably.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * This attribute is used during documentation generation to write the introduction section.
  */
 @Introduction("This plugin is used to add additional JSON parsing functionality to Kafka Connect.")
