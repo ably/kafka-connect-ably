17d2a2c8224ff3eda7d186ba6ed9ab09cd7e335d
ikbalkaya <ceikbal@gmail.com>
2022-02-28 10:28:10 +0000

    Fix channel type

--- src/main/java/com/ably/kafka/connect/ChannelSinkMapping.java
+++ src/main/java/com/ably/kafka/connect/ChannelSinkMapping.java
@@ -1,9 +1,9 @@
 package com.ably.kafka.connect;
 
 import io.ably.lib.realtime.AblyRealtime;
+import io.ably.lib.realtime.Channel;
+import io.ably.lib.types.AblyException;
 import org.apache.kafka.connect.sink.SinkRecord;
-
-import java.nio.channels.Channel;
 
 public interface ChannelSinkMapping {
     /**
@@ -13,5 +13,5 @@ public interface ChannelSinkMapping {
      * @param ablyRealtime AblyRealtime instance.
      * @return The channel.
      */
-    Channel getChannel(SinkRecord sinkRecord, AblyRealtime ablyRealtime);
+    Channel getChannel(SinkRecord sinkRecord, AblyRealtime ablyRealtime) throws AblyException;
 }
