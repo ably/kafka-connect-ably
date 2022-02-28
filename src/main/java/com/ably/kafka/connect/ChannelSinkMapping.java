b244a951a0f1e8aa7da75cc98be9dfb2a6748600
ikbalkaya <ceikbal@gmail.com>
2022-02-28 10:21:29 +0000

    Add AblyRealtime as parameter of getChannel

--- src/main/java/com/ably/kafka/connect/ChannelSinkMapping.java
+++ src/main/java/com/ably/kafka/connect/ChannelSinkMapping.java
@@ -1,5 +1,6 @@
 package com.ably.kafka.connect;
 
+import io.ably.lib.realtime.AblyRealtime;
 import org.apache.kafka.connect.sink.SinkRecord;
 
 import java.nio.channels.Channel;
@@ -9,7 +10,8 @@ public interface ChannelSinkMapping {
      * Returns the channel for the given sink record.
      *
      * @param sinkRecord The sink record.
+     * @param ablyRealtime AblyRealtime instance.
      * @return The channel.
      */
-    Channel getChannel(SinkRecord sinkRecord);
+    Channel getChannel(SinkRecord sinkRecord, AblyRealtime ablyRealtime);
 }
--- src/main/java/com/ably/kafka/connect/DefaultChannelSinkMapping.java
+++ src/main/java/com/ably/kafka/connect/DefaultChannelSinkMapping.java
@@ -1,12 +1,13 @@
 package com.ably.kafka.connect;
 
+import io.ably.lib.realtime.AblyRealtime;
 import org.apache.kafka.connect.sink.SinkRecord;
 
 import java.nio.channels.Channel;
 
 public class DefaultChannelSinkMapping implements ChannelSinkMapping {
     @Override
-    public Channel getChannel(SinkRecord sinkRecord) {
+    public Channel getChannel(SinkRecord sinkRecord, AblyRealtime ablyRealtime) {
         return null;
     }
 }
