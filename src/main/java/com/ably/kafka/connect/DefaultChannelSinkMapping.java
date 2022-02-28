189ba62e500db5ba5acd45d554f20e96181d593a
ikbalkaya <ceikbal@gmail.com>
2022-02-28 10:28:42 +0000

    Add default static configuration for channel

--- src/main/java/com/ably/kafka/connect/DefaultChannelSinkMapping.java
+++ src/main/java/com/ably/kafka/connect/DefaultChannelSinkMapping.java
@@ -1,13 +1,18 @@
 package com.ably.kafka.connect;
 
 import io.ably.lib.realtime.AblyRealtime;
+import io.ably.lib.realtime.Channel;
+import io.ably.lib.types.AblyException;
 import org.apache.kafka.connect.sink.SinkRecord;
 
-import java.nio.channels.Channel;
-
 public class DefaultChannelSinkMapping implements ChannelSinkMapping {
+    private final ChannelSinkConnectorConfig sinkConnectorConfig;
+    public DefaultChannelSinkMapping(ChannelSinkConnectorConfig config) {
+        sinkConnectorConfig = config;
+    }
+
     @Override
-    public Channel getChannel(SinkRecord sinkRecord, AblyRealtime ablyRealtime) {
-        return null;
+    public Channel getChannel(SinkRecord sinkRecord, AblyRealtime ablyRealtime) throws AblyException {
+        return ablyRealtime.channels.get(sinkConnectorConfig.channelName, sinkConnectorConfig.channelOptions);
     }
 }
