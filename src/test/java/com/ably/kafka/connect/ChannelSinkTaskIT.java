48237585961bc5dca410156e01cd48ab75e53735
ikbalkaya <ceikbal@gmail.com>
2022-03-18 11:47:05 +0000

    Add test for topic placeholder

--- src/test/java/com/ably/kafka/connect/ChannelSinkTaskIT.java
+++ src/test/java/com/ably/kafka/connect/ChannelSinkTaskIT.java
@@ -38,12 +38,14 @@ import static org.apache.kafka.connect.runtime.SinkConnectorConfig.TOPICS_CONFIG
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChannelSinkTaskIT {
-    private static final String CONNECTOR_NAME = "ably-test";
+    private static final String CONNECTOR_NAME = "ably-test-connector";
     private static final String SINK_CONNECTOR_CLASS_NAME = ChannelSinkConnector.class.getSimpleName();
     private static final int NUM_WORKERS = 1;
     private static final int NUM_TASKS = 1;
+    public static final long TIMEOUT = 5000L;
+    private static final String TOPICS = "topic1,topic2,topic3";
 
-    private EmbeddedConnectCluster connect;
+    private EmbeddedConnectCluster connectCluster;
     private AblyHelpers.AppSpec appSpec;
     private AblyRealtime ablyClient;
 
@@ -53,11 +55,11 @@ public class ChannelSinkTaskIT {
         appSpec = AblyHelpers.createTestApp();
 
         // setup Connect cluster with defaults
-        connect = new EmbeddedConnectCluster.Builder().build();
+        connectCluster = new EmbeddedConnectCluster.Builder().build();
 
         // start Connect cluster
-        connect.start();
-        connect.assertions().assertAtLeastNumWorkersAreUp(NUM_WORKERS, "Initial group of workers did not start in time.");
+        connectCluster.start();
+        connectCluster.assertions().assertAtLeastNumWorkersAreUp(NUM_WORKERS, "Initial group of workers did not start in time.");
 
         // connect Ably client
         ablyClient = AblyHelpers.realtimeClient(appSpec);
@@ -65,36 +67,63 @@ public class ChannelSinkTaskIT {
 
     @AfterEach
     public void close() {
-        connect.stop();
+        connectCluster.stop();
         ablyClient.close();
         AblyHelpers.deleteTestApp(appSpec);
     }
 
     @Test
-    public void testMessagePublishMessageReceived() throws Exception {
-        // create a test Kafka topic
-        connect.kafka().createTopic("kafka-connect-ably-test");
+    public void testMessagePublishMessageReceivedWithStaticChannelName() throws Exception {
+        final String channelName = "kafka-connect-ably-test";
+        // topic1
+        final String topic = TOPICS.split(",")[0];
+        connectCluster.kafka().createTopic(topic);
 
-        // configure the Ably connector to publish messages from the test Kafka
-        // topic to an Ably channel
-        Map<String, String> settings = createSettings("kafka-connect-ably-test",null ,null ,null );
-        connect.configureConnector(CONNECTOR_NAME, settings);
-        connect.assertions().assertConnectorAndAtLeastNumTasksAreRunning(CONNECTOR_NAME, NUM_TASKS, "Connector tasks did not start in time.");
+        Map<String, String> settings = createSettings(channelName,null ,null ,null );
+        connectCluster.configureConnector(CONNECTOR_NAME, settings);
+        connectCluster.assertions().assertConnectorAndAtLeastNumTasksAreRunning(CONNECTOR_NAME, NUM_TASKS, "Connector tasks did not start in time.");
 
         // subscribe to the Ably channel
-        Channel channel = ablyClient.channels.get("kafka-connect-ably-test");
+        Channel channel = ablyClient.channels.get(channelName);
         AblyHelpers.MessageWaiter messageWaiter = new AblyHelpers.MessageWaiter(channel);
 
         // produce a message on the Kafka topic
-        connect.kafka().produce("kafka-connect-ably-test", "foo", "bar");
+        connectCluster.kafka().produce(topic, "foo", "bar");
 
         // wait 5s for the message to arrive on the Ably channel
-        messageWaiter.waitFor(1, 5000L);
+        messageWaiter.waitFor(1, TIMEOUT);
         final List<Message> receivedMessages = messageWaiter.receivedMessages;
         assertEquals(receivedMessages.size(), 1, "Unexpected message count");
 
         // delete connector
-        connect.deleteConnector(CONNECTOR_NAME);
+        connectCluster.deleteConnector(CONNECTOR_NAME);
+    }
+
+    @Test
+    public void testMessagePublish_MessageReceivedWithTopicPlaceholder() throws Exception {
+        // topic1
+        final String topic = TOPICS.split(",")[0];
+        connectCluster.kafka().createTopic(topic);
+
+        final String topicedChannelName = "#{topic}_channel";
+        Map<String, String> settings = createSettings(topicedChannelName,null ,null ,null );
+        connectCluster.configureConnector(CONNECTOR_NAME, settings);
+        connectCluster.assertions().assertConnectorAndAtLeastNumTasksAreRunning(CONNECTOR_NAME, NUM_TASKS, "Connector tasks did not start in time.");
+
+        // subscribe to the Ably channel
+        Channel channel = ablyClient.channels.get("topic1_channel");
+        AblyHelpers.MessageWaiter messageWaiter = new AblyHelpers.MessageWaiter(channel);
+
+        // produce a message on the Kafka topic
+        connectCluster.kafka().produce(topic, "foo", "bar");
+
+        // wait 5s for the message to arrive on the Ably channel
+        messageWaiter.waitFor(1, TIMEOUT);
+        final List<Message> receivedMessages = messageWaiter.receivedMessages;
+        assertEquals(receivedMessages.size(), 1, "Unexpected message count");
+
+        // delete connector
+        connectCluster.deleteConnector(CONNECTOR_NAME);
     }
 
     //let's use this method to create different settings
@@ -102,7 +131,7 @@ public class ChannelSinkTaskIT {
         Map<String, String> settings = new HashMap<>();
         settings.put(CONNECTOR_CLASS_CONFIG, SINK_CONNECTOR_CLASS_NAME);
         settings.put(TASKS_MAX_CONFIG, String.valueOf(NUM_TASKS));
-        settings.put(TOPICS_CONFIG, "kafka-connect-ably-test");
+        settings.put(TOPICS_CONFIG, TOPICS);
         settings.put(KEY_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
         settings.put(VALUE_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
         settings.put(ChannelSinkConnectorConfig.CHANNEL_CONFIG, channel);
