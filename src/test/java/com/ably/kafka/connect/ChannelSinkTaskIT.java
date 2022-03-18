d6364ffc536160987cf6fad0b0a89cd49d1455cd
ikbalkaya <ceikbal@gmail.com>
2022-03-18 09:55:59 +0000

    Refactor integration test class to make it easy to create different settings

--- src/test/java/com/ably/kafka/connect/ChannelSinkTaskIT.java
+++ src/test/java/com/ably/kafka/connect/ChannelSinkTaskIT.java
@@ -16,29 +16,26 @@
 
 package com.ably.kafka.connect;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.kafka.connect.converters.ByteArrayConverter;
-import org.apache.kafka.connect.util.clusters.EmbeddedConnectCluster;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-
 import io.ably.lib.realtime.AblyRealtime;
 import io.ably.lib.realtime.Channel;
-import io.ably.lib.realtime.Channel.MessageListener;
-import io.ably.lib.types.ClientOptions;
 import io.ably.lib.types.Message;
+import org.apache.kafka.connect.converters.ByteArrayConverter;
+import org.apache.kafka.connect.util.clusters.EmbeddedConnectCluster;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.apache.kafka.connect.runtime.ConnectorConfig.CONNECTOR_CLASS_CONFIG;
 import static org.apache.kafka.connect.runtime.ConnectorConfig.KEY_CONVERTER_CLASS_CONFIG;
 import static org.apache.kafka.connect.runtime.ConnectorConfig.TASKS_MAX_CONFIG;
 import static org.apache.kafka.connect.runtime.ConnectorConfig.VALUE_CONVERTER_CLASS_CONFIG;
 import static org.apache.kafka.connect.runtime.SinkConnectorConfig.TOPICS_CONFIG;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChannelSinkTaskIT {
     private static final String CONNECTOR_NAME = "ably-test";
@@ -74,22 +71,13 @@ public class ChannelSinkTaskIT {
     }
 
     @Test
-    public void testMessagePublish() throws Exception {
+    public void testMessagePublishMessageReceived() throws Exception {
         // create a test Kafka topic
         connect.kafka().createTopic("kafka-connect-ably-test");
 
         // configure the Ably connector to publish messages from the test Kafka
         // topic to an Ably channel
-        Map<String, String> settings = new HashMap<>();
-        settings.put(CONNECTOR_CLASS_CONFIG, SINK_CONNECTOR_CLASS_NAME);
-        settings.put(TASKS_MAX_CONFIG, String.valueOf(NUM_TASKS));
-        settings.put(TOPICS_CONFIG, "kafka-connect-ably-test");
-        settings.put(KEY_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
-        settings.put(VALUE_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
-        settings.put(ChannelSinkConnectorConfig.CHANNEL_CONFIG, "kafka-connect-ably-test");
-        settings.put(ChannelSinkConnectorConfig.CLIENT_KEY, appSpec.key());
-        settings.put(ChannelSinkConnectorConfig.CLIENT_ID, "kafka-connect-ably-test");
-        settings.put(ChannelSinkConnectorConfig.CLIENT_ENVIRONMENT, AblyHelpers.TEST_ENVIRONMENT);
+        Map<String, String> settings = createSettings("kafka-connect-ably-test",null ,null ,null );
         connect.configureConnector(CONNECTOR_NAME, settings);
         connect.assertions().assertConnectorAndAtLeastNumTasksAreRunning(CONNECTOR_NAME, NUM_TASKS, "Connector tasks did not start in time.");
 
@@ -102,9 +90,34 @@ public class ChannelSinkTaskIT {
 
         // wait 5s for the message to arrive on the Ably channel
         messageWaiter.waitFor(1, 5000L);
-        assertEquals(messageWaiter.receivedMessages.size(), 1, "Unexpected message count");
+        final List<Message> receivedMessages = messageWaiter.receivedMessages;
+        assertEquals(receivedMessages.size(), 1, "Unexpected message count");
 
         // delete connector
         connect.deleteConnector(CONNECTOR_NAME);
+    }
+
+    //let's use this method to create different settings
+    private Map<String, String> createSettings(@Nonnull String channel, String cipherKey, String channelParams, String messageName) {
+        Map<String, String> settings = new HashMap<>();
+        settings.put(CONNECTOR_CLASS_CONFIG, SINK_CONNECTOR_CLASS_NAME);
+        settings.put(TASKS_MAX_CONFIG, String.valueOf(NUM_TASKS));
+        settings.put(TOPICS_CONFIG, "kafka-connect-ably-test");
+        settings.put(KEY_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
+        settings.put(VALUE_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
+        settings.put(ChannelSinkConnectorConfig.CHANNEL_CONFIG, channel);
+        settings.put(ChannelSinkConnectorConfig.CLIENT_KEY, appSpec.key());
+        settings.put(ChannelSinkConnectorConfig.CLIENT_ID, "kafka-connect-ably-test");
+        settings.put(ChannelSinkConnectorConfig.CLIENT_ENVIRONMENT, AblyHelpers.TEST_ENVIRONMENT);
+        if (cipherKey != null) {
+            settings.put(ChannelSinkConnectorConfig.CLIENT_CHANNEL_CIPHER_KEY, cipherKey);
+        }
+        if (channelParams != null) {
+            settings.put(ChannelSinkConnectorConfig.CLIENT_CHANNEL_PARAMS, channelParams);
+        }
+        if (messageName != null) {
+            settings.put(ChannelSinkConnectorConfig.MESSAGE_CONFIG, messageName);
+        }
+        return settings;
     }
 }
