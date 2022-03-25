8bc96e2749998220a9dead39a7fa66b37d5a55b0
ikbalkaya <ceikbal@gmail.com>
2022-03-25 10:01:26 +0000

    Add function to tweak required settings for tests

--- src/test/java/com/ably/kafka/connect/ChannelSinkConnectorIT.java
+++ src/test/java/com/ably/kafka/connect/ChannelSinkConnectorIT.java
@@ -1,17 +1,12 @@
 package com.ably.kafka.connect;
 
-import io.ably.lib.realtime.AblyRealtime;
-import io.ably.lib.realtime.Channel;
-import io.ably.lib.types.Message;
 import org.apache.kafka.connect.converters.ByteArrayConverter;
 import org.apache.kafka.connect.util.clusters.EmbeddedConnectCluster;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.Nonnull;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.apache.kafka.connect.runtime.ConnectorConfig.CONNECTOR_CLASS_CONFIG;
@@ -19,7 +14,6 @@ import static org.apache.kafka.connect.runtime.ConnectorConfig.KEY_CONVERTER_CLA
 import static org.apache.kafka.connect.runtime.ConnectorConfig.TASKS_MAX_CONFIG;
 import static org.apache.kafka.connect.runtime.ConnectorConfig.VALUE_CONVERTER_CLASS_CONFIG;
 import static org.apache.kafka.connect.runtime.SinkConnectorConfig.TOPICS_CONFIG;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChannelSinkConnectorIT {
 
@@ -55,7 +49,7 @@ public class ChannelSinkConnectorIT {
         final String topic = TOPICS.split(",")[0];
         connectCluster.kafka().createTopic(topic);
 
-        Map<String, String> settings = createSettings(channelName, null, null, null);
+        Map<String, String> settings = createSettings(channelName, "test-key", "some_client_id", TOPICS);
         connectCluster.configureConnector(CONNECTOR_NAME, settings);
         connectCluster.assertions().assertConnectorAndAtLeastNumTasksAreRunning(CONNECTOR_NAME, NUM_TASKS, "Connector tasks did not start in time.");
 
@@ -63,8 +57,13 @@ public class ChannelSinkConnectorIT {
         connectCluster.deleteConnector(CONNECTOR_NAME);
     }
 
-
-    private Map<String, String> createSettings(@Nonnull String channel, String cipherKey, String channelParams, String messageName) {
+   /* Settings that accepts all required properties as parameters,
+   * Use this to emulate error cases
+   * */
+    private Map<String, String> createSettings(String channel,
+                                               String clientKey,
+                                               String clientId,
+                                               String topics) {
         Map<String, String> settings = new HashMap<>();
         settings.put(CONNECTOR_CLASS_CONFIG, SINK_CONNECTOR_CLASS_NAME);
         settings.put(TASKS_MAX_CONFIG, String.valueOf(NUM_TASKS));
@@ -72,18 +71,9 @@ public class ChannelSinkConnectorIT {
         settings.put(KEY_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
         settings.put(VALUE_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
         settings.put(ChannelSinkConnectorConfig.CHANNEL_CONFIG, channel);
-        settings.put(ChannelSinkConnectorConfig.CLIENT_KEY, "appSpec.key()");
-        settings.put(ChannelSinkConnectorConfig.CLIENT_ID, "kafka-connect-ably-test");
+        settings.put(ChannelSinkConnectorConfig.CLIENT_KEY, clientKey);
+        settings.put(ChannelSinkConnectorConfig.CLIENT_ID, clientId);
         settings.put(ChannelSinkConnectorConfig.CLIENT_ENVIRONMENT, AblyHelpers.TEST_ENVIRONMENT);
-        if (cipherKey != null) {
-            settings.put(ChannelSinkConnectorConfig.CLIENT_CHANNEL_CIPHER_KEY, cipherKey);
-        }
-        if (channelParams != null) {
-            settings.put(ChannelSinkConnectorConfig.CLIENT_CHANNEL_PARAMS, channelParams);
-        }
-        if (messageName != null) {
-            settings.put(ChannelSinkConnectorConfig.MESSAGE_CONFIG, messageName);
-        }
         return settings;
     }
 }
