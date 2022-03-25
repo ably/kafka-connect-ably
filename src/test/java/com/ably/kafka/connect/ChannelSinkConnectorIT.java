27305223c4eb784f0a4c0b0f30b2a7968ce30891
ikbalkaya <ceikbal@gmail.com>
2022-03-25 09:38:35 +0000

    Add integration test class for connector

---
+++ src/test/java/com/ably/kafka/connect/ChannelSinkConnectorIT.java
@@ -0,0 +1,89 @@
+package com.ably.kafka.connect;
+
+import io.ably.lib.realtime.AblyRealtime;
+import io.ably.lib.realtime.Channel;
+import io.ably.lib.types.Message;
+import org.apache.kafka.connect.converters.ByteArrayConverter;
+import org.apache.kafka.connect.util.clusters.EmbeddedConnectCluster;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.kafka.connect.runtime.ConnectorConfig.CONNECTOR_CLASS_CONFIG;
+import static org.apache.kafka.connect.runtime.ConnectorConfig.KEY_CONVERTER_CLASS_CONFIG;
+import static org.apache.kafka.connect.runtime.ConnectorConfig.TASKS_MAX_CONFIG;
+import static org.apache.kafka.connect.runtime.ConnectorConfig.VALUE_CONVERTER_CLASS_CONFIG;
+import static org.apache.kafka.connect.runtime.SinkConnectorConfig.TOPICS_CONFIG;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChannelSinkConnectorIT {
+
+    private static final String CONNECTOR_NAME = "ably-test-connector";
+    private static final String SINK_CONNECTOR_CLASS_NAME = ChannelSinkConnector.class.getSimpleName();
+    private static final int NUM_WORKERS = 1;
+    private static final int NUM_TASKS = 1;
+    public static final long TIMEOUT = 5000L;
+    private static final String TOPICS = "topic1,topic2,topic3";
+
+    private EmbeddedConnectCluster connectCluster;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        // setup Connect cluster with defaults
+        connectCluster = new EmbeddedConnectCluster.Builder().build();
+
+        // start Connect cluster
+        connectCluster.start();
+        connectCluster.assertions().assertAtLeastNumWorkersAreUp(NUM_WORKERS, "Initial group of workers did not start in time.");
+
+    }
+
+    @AfterEach
+    public void close() {
+        connectCluster.stop();
+    }
+
+
+    @Test
+    public void testConnector_connectorWorksWithValidConfiguration() throws Exception {
+        final String channelName = "test-channel";
+        final String topic = TOPICS.split(",")[0];
+        connectCluster.kafka().createTopic(topic);
+
+        Map<String, String> settings = createSettings(channelName, null, null, null);
+        connectCluster.configureConnector(CONNECTOR_NAME, settings);
+        connectCluster.assertions().assertConnectorAndAtLeastNumTasksAreRunning(CONNECTOR_NAME, NUM_TASKS, "Connector tasks did not start in time.");
+
+        // delete connector
+        connectCluster.deleteConnector(CONNECTOR_NAME);
+    }
+
+
+    private Map<String, String> createSettings(@Nonnull String channel, String cipherKey, String channelParams, String messageName) {
+        Map<String, String> settings = new HashMap<>();
+        settings.put(CONNECTOR_CLASS_CONFIG, SINK_CONNECTOR_CLASS_NAME);
+        settings.put(TASKS_MAX_CONFIG, String.valueOf(NUM_TASKS));
+        settings.put(TOPICS_CONFIG, TOPICS);
+        settings.put(KEY_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
+        settings.put(VALUE_CONVERTER_CLASS_CONFIG, ByteArrayConverter.class.getName());
+        settings.put(ChannelSinkConnectorConfig.CHANNEL_CONFIG, channel);
+        settings.put(ChannelSinkConnectorConfig.CLIENT_KEY, "appSpec.key()");
+        settings.put(ChannelSinkConnectorConfig.CLIENT_ID, "kafka-connect-ably-test");
+        settings.put(ChannelSinkConnectorConfig.CLIENT_ENVIRONMENT, AblyHelpers.TEST_ENVIRONMENT);
+        if (cipherKey != null) {
+            settings.put(ChannelSinkConnectorConfig.CLIENT_CHANNEL_CIPHER_KEY, cipherKey);
+        }
+        if (channelParams != null) {
+            settings.put(ChannelSinkConnectorConfig.CLIENT_CHANNEL_PARAMS, channelParams);
+        }
+        if (messageName != null) {
+            settings.put(ChannelSinkConnectorConfig.MESSAGE_CONFIG, messageName);
+        }
+        return settings;
+    }
+}
