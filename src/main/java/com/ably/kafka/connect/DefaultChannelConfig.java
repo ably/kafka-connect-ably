5ab746257b1e1695cf79084ecca2c10f3180053a
ikbalkaya <ceikbal@gmail.com>
2022-03-25 15:27:23 +0000

    Add finals where neccessary and reduce code smells

--- src/main/java/com/ably/kafka/connect/DefaultChannelConfig.java
+++ src/main/java/com/ably/kafka/connect/DefaultChannelConfig.java
@@ -17,7 +17,7 @@ import static com.ably.kafka.connect.ChannelSinkConnectorConfig.CLIENT_CHANNEL_P
 class DefaultChannelConfig implements ChannelConfig {
     private final ChannelSinkConnectorConfig sinkConnectorConfig;
 
-    public DefaultChannelConfig(ChannelSinkConnectorConfig sinkConnectorConfig) {
+    public DefaultChannelConfig(final ChannelSinkConnectorConfig sinkConnectorConfig) {
         this.sinkConnectorConfig = sinkConnectorConfig;
     }
 
@@ -28,32 +28,32 @@ class DefaultChannelConfig implements ChannelConfig {
 
     public ChannelOptions getOptions() throws ChannelSinkConnectorConfig.ConfigException {
         final Logger logger = LoggerFactory.getLogger(ChannelSinkConnectorConfig.class);
-        ChannelOptions opts;
-        String cipherKey = sinkConnectorConfig.getString(CLIENT_CHANNEL_CIPHER_KEY);
+        ChannelOptions channelOptions;
+        final String cipherKey = sinkConnectorConfig.getString(CLIENT_CHANNEL_CIPHER_KEY);
 
         if (cipherKey != null && !cipherKey.trim().isEmpty()) {
             try {
-                opts = ChannelOptions.withCipherKey(cipherKey);
+                channelOptions = ChannelOptions.withCipherKey(cipherKey);
             } catch (AblyException e) {
                 logger.error("Error configuring channel cipher key", e);
                 throw new ChannelSinkConnectorConfig.ConfigException("Error configuring channel cipher key", e);
             }
         } else {
-            opts = new ChannelOptions();
+            channelOptions = new ChannelOptions();
         }
 
         // Since we're only publishing, set the channel mode to publish only
-        opts.modes = new ChannelMode[]{ChannelMode.publish};
-        opts.params = getParams(sinkConnectorConfig.getList(CLIENT_CHANNEL_PARAMS));
-        return opts;
+        channelOptions.modes = new ChannelMode[]{ChannelMode.publish};
+        channelOptions.params = getParams(sinkConnectorConfig.getList(CLIENT_CHANNEL_PARAMS));
+        return channelOptions;
     }
 
-    private Map<String, String> getParams(List<String> params) throws ChannelSinkConnectorConfig.ConfigException {
+    private Map<String, String> getParams(final List<String> params) throws ChannelSinkConnectorConfig.ConfigException {
         final Logger logger = LoggerFactory.getLogger(ChannelSinkConnectorConfig.class);
 
-        Map<String, String> parsedParams = new HashMap<String, String>();
-        for (String param : params) {
-            String[] parts = param.split("=");
+        final Map<String, String> parsedParams = new HashMap<>();
+        for (final String param : params) {
+            final String[] parts = param.split("=");
             if (parts.length == 2) {
                 final String paramKey = parts[0];
                 final String paramVal = parts[1];
