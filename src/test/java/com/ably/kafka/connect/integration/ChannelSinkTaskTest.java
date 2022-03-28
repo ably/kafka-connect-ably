aaebb05cade80b55b6d4fefe0ed8aa4b8fcaabf5
ikbalkaya <ceikbal@gmail.com>
2022-03-28 14:44:04 +0100

    Remove copyright wording from test class documentation

--- src/test/java/com/ably/kafka/connect/integration/ChannelSinkTaskTest.java
+++ src/test/java/com/ably/kafka/connect/integration/ChannelSinkTaskTest.java
@@ -1,6 +1,4 @@
 /*
-  Copyright © 2021 Ably Real-time Ltd. (support@ably.com)
-  <p>
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
