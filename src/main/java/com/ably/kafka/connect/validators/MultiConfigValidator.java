6496203f15dc5ab6ed790f01ab3f2f9bcf591c22
ikbalkaya <ceikbal@gmail.com>
2022-04-01 09:45:28 +0100

    Remove unneccessary if from ensureValid

--- src/main/java/com/ably/kafka/connect/validators/MultiConfigValidator.java
+++ src/main/java/com/ably/kafka/connect/validators/MultiConfigValidator.java
@@ -1,6 +1,7 @@
 package com.ably.kafka.connect.validators;
 
 import org.apache.kafka.common.config.ConfigDef;
+
 import javax.annotation.Nonnull;
 
 /*
@@ -8,21 +9,19 @@ Currently connector configurations take only a single validator per configuratio
 support multiple validators.
 * */
 public class MultiConfigValidator implements ConfigDef.Validator {
-   private final ConfigDef.Validator[] validators;
+    private final ConfigDef.Validator[] validators;
 
-   /**
-    * @param validators validators to use. Please provide them by their priority of importance.
-    */
+    /**
+     * @param validators validators to use. Please provide them by their priority of importance.
+     */
     public MultiConfigValidator(@Nonnull ConfigDef.Validator[] validators) {
         this.validators = validators;
     }
 
     @Override
     public void ensureValid(String name, Object value) {
-        if (validators.length > 0) {
-            for (ConfigDef.Validator validator : validators) {
-                validator.ensureValid(name, value);
-            }
+        for (ConfigDef.Validator validator : validators) {
+            validator.ensureValid(name, value);
         }
     }
 }
