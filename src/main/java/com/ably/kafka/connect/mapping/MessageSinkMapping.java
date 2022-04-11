b4e32af03ac371f451a48e28c39726df2be75eff
ikbalkaya <ceikbal@gmail.com>
2022-04-11 17:17:52 +0100

    Remove unnecessary imports

--- src/main/java/com/ably/kafka/connect/mapping/MessageSinkMapping.java
+++ src/main/java/com/ably/kafka/connect/mapping/MessageSinkMapping.java
@@ -1,16 +1,7 @@
 package com.ably.kafka.connect.mapping;
 
-import com.ably.kafka.connect.utils.KafkaExtrasExtractor;
-import com.ably.kafka.connect.config.ChannelSinkConnectorConfig;
-import com.ably.kafka.connect.config.ConfigValueEvaluator;
 import io.ably.lib.types.Message;
-import io.ably.lib.types.MessageExtras;
-import io.ably.lib.util.JsonUtils;
 import org.apache.kafka.connect.sink.SinkRecord;
-
-import javax.annotation.Nonnull;
-
-import static com.ably.kafka.connect.config.ChannelSinkConnectorConfig.MESSAGE_CONFIG;
 
 public interface MessageSinkMapping {
     Message getMessage(SinkRecord record);
