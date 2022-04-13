b9758a78281b462c9328fa5e6e645f35b4bd7699
ikbalkaya <ceikbal@gmail.com>
2022-04-13 11:55:59 +0100

    Add documentation for create() on AblyClientFactory

--- src/main/java/com/ably/kafka/connect/client/AblyClientFactory.java
+++ src/main/java/com/ably/kafka/connect/client/AblyClientFactory.java
@@ -5,5 +5,13 @@ import com.ably.kafka.connect.config.ChannelSinkConnectorConfig;
 import java.util.Map;
 
 public interface AblyClientFactory {
+    /**
+     * Create a new AblyClient instance.
+     *
+     * @param settings The connector configuration for the client.
+     * @return The client.
+     *
+     * @throws ChannelSinkConnectorConfig.ConfigException If the client cannot be created.
+     */
     AblyClient create(Map<String, String> settings)  throws ChannelSinkConnectorConfig.ConfigException;
 }
