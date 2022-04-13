cc9e1b154787b0296e9f61071aa71d1c5d6128e5
ikbalkaya <ceikbal@gmail.com>
2022-04-13 12:19:22 +0100

    Remove unneccesary import

--- src/main/java/com/ably/kafka/connect/client/DefaultAblyClient.java
+++ src/main/java/com/ably/kafka/connect/client/DefaultAblyClient.java
@@ -16,8 +16,6 @@ import org.apache.kafka.connect.sink.SinkRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.CountDownLatch;
-
 public class DefaultAblyClient implements AblyClient {
     private static final Logger logger = LoggerFactory.getLogger(DefaultAblyClient.class);
 
