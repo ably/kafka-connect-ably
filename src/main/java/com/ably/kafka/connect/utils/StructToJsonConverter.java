4a27b1d5bb179152d820af7555e33f30ccef309e
ikbalkaya <ceikbal@gmail.com>
2022-05-24 16:35:28 +0100

    Refactor and code documentation for StructToJsonConverter

--- src/main/java/com/ably/kafka/connect/utils/StructToJsonConverter.java
+++ src/main/java/com/ably/kafka/connect/utils/StructToJsonConverter.java
@@ -9,37 +9,63 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class is used to convert a Struct to a JSON string with eliminating the schema information.
+ * This class uses the Gson library tow convet the Struct to a JSON string.
+ * */
 public class StructToJsonConverter {
+    /**
+     * Convert a Struct to a JSON string eliminating the schema information.
+     * @param struct the Struct to convert.
+     * @return the JSON string.
+     * */
     public static String toJsonString(final Struct struct, Gson gson) {
-        return gson.toJson(structMap(struct));
+        final Map<String, Object> structJsonMap = structJsonMap(struct);
+        return gson.toJson(structJsonMap);
     }
 
-    private static Map<String, Object> structMap(final Struct struct) {
-        final Map<String, Object> dataMap = new LinkedHashMap<>(struct.schema().fields().size());
+    /**
+     * Convert a Struct to a LinkedHashMap by eliminating schematic information and
+     * mapping field names to corresponding values. This function is recursive and the built map will reflect
+     * the shape of data in the Struct without schema information.
+     *
+     * @param struct Struct to convert
+     * @return Map that represents shape of data in the Struct without schema information
+     */
+    private static Map<String, Object> structJsonMap(final Struct struct) {
+        final Map<String, Object> structMap = new LinkedHashMap<>(struct.schema().fields().size());
 
         for (Field field : struct.schema().fields()) {
             switch (field.schema().type()) {
                 case STRUCT:
                     final Struct fieldStruct = struct.getStruct(field.name());
-                    dataMap.put(field.name(), structMap(fieldStruct));
+                    structMap.put(field.name(), structJsonMap(fieldStruct));
                     break;
                 case ARRAY:
                     final List<Object> fieldArray = struct.getArray(field.name());
-                    final List<Object> fieldJsonArray = jsonArrayFrom(fieldArray);
-                    dataMap.put(field.name(), fieldJsonArray);
+                    final List<Object> fieldJsonArray = jsonArrayFromStructArray(fieldArray);
+                    structMap.put(field.name(), fieldJsonArray);
                     break;
                 case MAP:
-                    final Map<String, Object> jsonMap = mapFrom(struct.getMap(field.name()));
-                    dataMap.put(field.name(), jsonMap);
+                    final Map<String, Object> jsonMap = jsonMapFromStructMap(struct.getMap(field.name()));
+                    structMap.put(field.name(), jsonMap);
                 default:
-                    dataMap.put(field.name(), jsonValue(struct.get(field)));
+                    structMap.put(field.name(), jsonValue(struct.get(field)));
             }
         }
-        return dataMap;
+        return structMap;
     }
 
-
-    private static Map<String, Object> mapFrom(Map<Object, Object> map) {
+    /**
+     * Convert a map from a map from struct (Struct.getMap()) to a
+     * LinkedHashMap by eliminating schematic information and evaluating the value of each key.
+     * Like [structJsonMap] this function  will build a map that reflects the shape of data in the
+     * original map without schema information.
+     *
+     * @param map Original map that is retrieved from Struct.getMap()
+     * @return Map that represents shape of data in the original map without schema information
+     */
+    private static Map<String, Object> jsonMapFromStructMap(Map<Object, Object> map) {
         if (map == null) {
             return null;
         }
@@ -50,7 +76,15 @@ public class StructToJsonConverter {
         return jsonMap;
     }
 
-    private static List<Object> jsonArrayFrom(List<Object> fieldArray) {
+    /**
+     * Convert a list of struct array (Struct.getArray()) to a plain list that can be JSON serialized. with
+     * enumarating on the original array and making a list of json serializable objects.
+     *
+     * @param fieldArray Original list of struct array
+     * @return List of JSOn serializable objects
+     */
+
+    private static List<Object> jsonArrayFromStructArray(List<Object> fieldArray) {
         if (fieldArray == null) {
             return null;
         }
@@ -61,14 +95,23 @@ public class StructToJsonConverter {
         return fieldJsonArray;
     }
 
-    private static Object jsonValue(final Object value) {
-        if (value instanceof Struct) {
-            return structMap((Struct) value);
-        } else if (value instanceof List) {
-            return jsonArrayFrom((List<Object>) value);
-        } else if (value instanceof Map) {
-            return mapFrom((Map<Object, Object>) value);
+    /**
+     * Get a Json serializable value from a value based on the type of original value.
+     * Depending on the typo of the original value, this function can return a map from a struct,
+     * a list from  a list of values, a map from a struct map or the value itself.
+     *
+     * @param originalValue Original value
+     * @return JSON serializable object
+     */
+
+    private static Object jsonValue(final Object originalValue) {
+        if (originalValue instanceof Struct) {
+            return structJsonMap((Struct) originalValue);
+        } else if (originalValue instanceof List) {
+            return jsonArrayFromStructArray((List<Object>) originalValue);
+        } else if (originalValue instanceof Map) {
+            return jsonMapFromStructMap((Map<Object, Object>) originalValue);
         }
-        return value;
+        return originalValue;
     }
 }
