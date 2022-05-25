6677e7b71b91495e9ee370bd06a8463322c50a29
ikbalkaya <ceikbal@gmail.com>
2022-05-25 10:06:58 +0100

    Add commentary to helper class

--- src/test/java/com/ably/kafka/connect/utils/AvroToStruct.java
+++ src/test/java/com/ably/kafka/connect/utils/AvroToStruct.java
@@ -28,6 +28,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
+/*
+This is a helper class that contains some conversions between avro and struct and some fake test data classes.
+* */
+
 public class AvroToStruct {
 
     Struct getStruct(final Object object) throws RestClientException, IOException {
