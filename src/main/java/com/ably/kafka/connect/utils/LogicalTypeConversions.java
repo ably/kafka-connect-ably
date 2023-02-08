b8e725539b41e2dacc636327a65c2e6bb3e9f75c
ikbalkaya <ceikbal@gmail.com>
2023-02-08 09:35:43 +0000

    Bring types in line with apache.avro.data conversions

--- src/main/java/com/ably/kafka/connect/utils/LogicalTypeConversions.java
+++ src/main/java/com/ably/kafka/connect/utils/LogicalTypeConversions.java
@@ -1,25 +1,15 @@
 package com.ably.kafka.connect.utils;
 
-import org.apache.kafka.connect.data.Date;
 import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.Time;
-import org.apache.kafka.connect.data.Timestamp;
 
 import java.math.BigDecimal;
-import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.util.UUID;
-/***
- * decimal -> This applies to bytes. This is BigDecimal
- * date ->  This applies to integers and is represented as LocalDate of Java
- * time-millis -> This applies to integers and is represented as LocalTime of Java.
- * time-micros -> Same as above
- * timestamp-millis -> Applies to long and is represented as LocalDateTime of Java
- * timestamp-micros -> Same as above
- * uuid -> Applies to Strings and is represented as UUID of Java
- * */
+import java.util.concurrent.TimeUnit;
 public class LogicalTypeConversions{
     /*
     *This method will try to get logical type of
@@ -36,32 +26,34 @@ public class LogicalTypeConversions{
                  if (value instanceof LocalDate) {
                      // date ->  This applies to integers and is represented as LocalDate of Java
                      final LocalDate date = (LocalDate) value;
-                     return date.toEpochDay();
+                     return (int)date.toEpochDay();
                  } else if (value instanceof LocalTime) {
                      //This applies to time-millis and time-micros
                      final LocalTime time = (LocalTime) value;
-                     return time.toSecondOfDay();// todo check this
+                     return (int) TimeUnit.NANOSECONDS.toMillis(time.toNanoOfDay());
                  }
             case INT64:
-                 if (value instanceof LocalDateTime) {
+                 if (value instanceof Instant) {
                      //timestamp-millis and timestamp-micros
-                    final LocalDateTime time = (LocalDateTime) value;
-                    return time.getSecond(); //todo check this
-                }else if (value instanceof Duration){
-                     //duration ? //todo check if such a thing exists
-                    final Duration duration = (Duration) value;
-                    duration.getNano();
-                }
+                    final Instant time = (Instant) value;
+                    return time.toEpochMilli();
+                } else if (value instanceof LocalDateTime) {
+                     //local-timestamp-millis and local-timestamp-micros
+                     final LocalDateTime localDateTime = (LocalDateTime) value;
+                     final Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
+                     return instant.toEpochMilli();
+                 }
+                 //duration is not implemented in org.apache.avro.data package - so leaving it here for now
                 break;
             case STRING:
-                //uuid
+                //logical type: uuid
                 if (value instanceof UUID){
                     final UUID uuid = (UUID) value;
                     return uuid.toString();
                 }
                 break;
             case BYTES:
-                //decimal
+                //logical type: decimal
                 if (value instanceof BigDecimal) {
                     //decimal - no need for conversion
                     return value;
