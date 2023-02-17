1918c37cf4bf6bb6c23d26869153db9b0ba24eba
ikbalkaya <ceikbal@gmail.com>
2023-02-17 16:21:27 +0000

    Add a wrapper and builder to simplify building message extras

--- src/main/java/com/ably/kafka/connect/utils/RecordHeaderConversions.java
+++ src/main/java/com/ably/kafka/connect/utils/RecordHeaderConversions.java
@@ -2,17 +2,16 @@ package com.ably.kafka.connect.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.google.gson.internal.LinkedTreeMap;
 import io.ably.lib.types.MessageExtras;
 import io.ably.lib.util.JsonUtils;
 import org.apache.kafka.connect.header.Header;
+import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.util.Base64;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RecordHeaderConversions {
@@ -20,6 +19,7 @@ public class RecordHeaderConversions {
     private static final Logger logger = LoggerFactory.getLogger(RecordHeaderConversions.class);
     private static final String PUSH_HEADER = "com.ably.extras.push";
     private static final String KAFKA_KEY = "kafka";
+    private static final String HEADERS_KEY = "headers";
     private static final String PUSH_KEY = "push";
     /**
      * Returns the extras object to use when converting a Kafka message
@@ -38,99 +38,137 @@ public class RecordHeaderConversions {
      */
     @Nullable
     public static MessageExtras toMessageExtras(final SinkRecord record) {
-        JsonUtils.JsonUtilsObject kafkaExtras = null;
-        JsonUtils.JsonUtilsObject extrasObject = null;
+       final Extras.Builder extrasBuilder = new Extras.Builder();
+        final Extras extras = extrasBuilder.key((byte[]) record.key())
+            .recordHeaders(record.headers())
+            .build();
 
-        Object pushExtras = null;
-        final byte[] key = (byte[]) record.key();
-        if (key != null) {
-            kafkaExtras = JsonUtils.object();
-            final String keyValue = Base64.getEncoder().encodeToString(key);
-            kafkaExtras.add("key", keyValue);
-        }
-
-        if (record.headers().isEmpty()){
-            if (kafkaExtras == null) {
-                return null;
-            }
-            extrasObject = JsonUtils.object();
-            extrasObject.add(KAFKA_KEY, kafkaExtras);
-            return new MessageExtras(extrasObject.toJson());
-        }
-
-        JsonUtils.JsonUtilsObject headersObject = null;
-        for (Header header : record.headers()) {
-            if (header.key().equals(PUSH_HEADER)) {
-                pushExtras = header.value();
-            } else {
-                if (kafkaExtras == null) {
-                    kafkaExtras = JsonUtils.object();
-                }
-                if (headersObject == null) {
-                    headersObject = JsonUtils.object();
-                }
-                headersObject.add(header.key(), header.value());
-            }
-        }
-
-        if (kafkaExtras == null && pushExtras == null) {
-            return null;
-        }
-
-        if (kafkaExtras != null && headersObject != null) {
-            kafkaExtras.add("headers", headersObject);
-        }
-
-        if (kafkaExtras != null) {
-            extrasObject = JsonUtils.object();
-            extrasObject.add(KAFKA_KEY, kafkaExtras);
-        }
-
-        if (pushExtras != null) {
-            String pushExtrasJson = null;
-            if (pushExtras instanceof String){
-                pushExtrasJson = (String) pushExtras;
-            } else if (pushExtras instanceof Map) {
-                pushExtrasJson = new Gson().toJson(pushExtras, Map.class);
-            }
-            if (pushExtrasJson != null){
-                final JsonObject pushPayload = buildPushPayload(pushExtrasJson);
-                if (pushPayload != null) {
-                    if (extrasObject == null) {
-                        extrasObject = JsonUtils.object();
-                    }
-                    extrasObject.add(PUSH_KEY, pushPayload);
-                }
-            }
-        }
-
-        if (extrasObject != null){
-            return new MessageExtras(extrasObject.toJson());
-        }
-
-        return null;
+        return extras.toMessageExtras();
     }
-    /**
-     * Validate and build push payload using given pushJson
-     * @param pushJson json string payload to validate and create a valid payload from
-     * */
-    private static JsonObject buildPushPayload(final String pushJson){
-        final Gson gson = new Gson();
-        final Map<String,Object> map = gson.fromJson(pushJson, Map.class);
-        if (map.get("notification") == null){
-            logger.error("Push payload is invalid : No 'notification' field was found");
-            return null;
+
+    /*
+    Wrapper class representing extras and is to be used to simplify building of extras object
+    * */
+    private static class Extras {
+        private JsonUtils.JsonUtilsObject kafkaObject;
+        private JsonUtils.JsonUtilsObject topObject;
+        private Object pushExtrasValue;
+
+        private JsonUtils.JsonUtilsObject headersObject = null;
+
+        static class Builder {
+            final Extras extras;
+            Builder() {
+                extras = new Extras();
+            }
+            Extras build() {
+                return extras;
+            }
+
+            Extras.Builder key(byte[] key) {
+                if (key != null) {
+                    kafkaExtras().add("key", Base64.getEncoder().encodeToString(key));
+                    topExtrasObject().add(KAFKA_KEY, kafkaExtras());
+                }
+
+                return this;
+            }
+
+            Extras.Builder recordHeaders(Headers headers) {
+                if (!headers.isEmpty()) {
+                    buildFromHeaders(headers);
+                }
+
+                return this;
+            }
+
+            private void buildFromHeaders(Headers headers) {
+                for (Header header : headers) {
+                    if (header.key().equals(PUSH_HEADER)) {
+                        extras.pushExtrasValue = header.value();
+                    } else {
+                        headersObject().add(header.key(), header.value());
+                    }
+                }
+
+                if (extras.headersObject != null) {
+                    kafkaExtras().add(HEADERS_KEY, extras.headersObject);
+                }
+
+                buildPushExtras();
+            }
+
+            private void buildPushExtras() {
+                if (extras.pushExtrasValue != null) {
+                    String pushExtrasJson = null;
+                    if (extras.pushExtrasValue instanceof String) {
+                        pushExtrasJson = (String) extras.pushExtrasValue;
+                    } else if (extras.pushExtrasValue instanceof Map) {
+                        pushExtrasJson = new Gson().toJson(extras.pushExtrasValue, Map.class);
+                    }
+                    if (pushExtrasJson != null) {
+                        final JsonObject pushPayload = buildPushPayload(pushExtrasJson);
+                        if (pushPayload != null) {
+                            topExtrasObject().add(PUSH_KEY, pushPayload);
+                        }
+                    }
+                }
+            }
+
+            /**
+             * Validate and build push payload using given pushJson
+             *
+             * @param pushJson json string payload to validate and create a valid payload from
+             */
+            private JsonObject buildPushPayload(final String pushJson) {
+                final Gson gson = new Gson();
+                final Map<String, Object> map = gson.fromJson(pushJson, Map.class);
+                if (map.get("notification") == null) {
+                    logger.error("Push payload is invalid : No 'notification' field was found");
+                    return null;
+                }
+                final Map notification = (Map) map.get("notification");
+                if (notification.get("title") == null) {
+                    logger.error("Push payload is invalid : No 'title' for notification was found");
+                    return null;
+                }
+
+                if (notification.get("body") == null) {
+                    logger.error("Push payload is invalid : No 'body' for notification was found");
+                    return null;
+                }
+                return gson.toJsonTree(map).getAsJsonObject();
+            }
+
+            private JsonUtils.JsonUtilsObject kafkaExtras() {
+                if (extras.kafkaObject == null) {
+                    extras.kafkaObject = JsonUtils.object();
+                }
+                return extras.kafkaObject;
+            }
+            private JsonUtils.JsonUtilsObject topExtrasObject() {
+                if (extras.topObject == null) {
+                    extras.topObject = JsonUtils.object();
+                }
+                return extras.topObject;
+            }
+
+            private JsonUtils.JsonUtilsObject headersObject() {
+                if (extras.headersObject == null) {
+                    extras.headersObject = JsonUtils.object();
+                }
+                return extras.headersObject;
+            }
+
         }
-        final Map notification = (Map) map.get("notification");
-        if (notification.get("title") == null){
-            logger.error("Push payload is invalid : No 'title' for notification was found");
+
+        @Nullable
+        MessageExtras toMessageExtras(){
+            if (topObject != null){
+                return new MessageExtras(topObject.toJson());
+            }
             return null;
         }
 
-        if (notification.get("body") == null){
-            logger.error("Push payload is invalid : No 'body' for notification was found");
-            return null;
-        }
-        return gson.toJsonTree(map).getAsJsonObject();
     }
 }
