eb8adcb8dbd245f9344268f6ae0c2065605fd388
ikbalkaya <ceikbal@gmail.com>
2023-03-03 11:21:20 +0000

    Replace access token with generic value
    Previously I had mistakenly added my personal access token which I later removed from Github, so it should no longer be valid

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,3 +1,3 @@
 github_changelog_generator -u ably -p kafka-connect-ably since-tag v2.1.1 \
 --output delta.md \
---token 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
+--token <Replace with personal access token>
