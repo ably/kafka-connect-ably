5838ce45b6237e136a55cea6e9414ef1df181e1c
James Aley <james.aley@gmail.com>
2023-06-27 09:19:11 +0100

    Merge DLQ and offset committing changes and resolve conflicts

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn clean package -DskipTests=true
+mvn clean package -DskipTests=true -Dmaven.javadoc.skip=true
 docker build -f Dockerfile_development . -t connector
--- src/main/java/com/ably/kafka/connect/batch/BatchProcessingThread.java
+++ src/main/java/com/ably/kafka/connect/batch/BatchProcessingThread.java
@@ -1,6 +1,8 @@
 package com.ably.kafka.connect.batch;
 
 import com.ably.kafka.connect.client.DefaultAblyBatchClient;
+import org.apache.kafka.connect.sink.ErrantRecordReporter;
+import com.ably.kafka.connect.offset.OffsetRegistry;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,18 +14,26 @@ import java.util.List;
  * is to call the Ably BATCH REST API
  * for every batch.(its grouped by channel)
  */
-public class BatchProcessingThread implements Runnable{
+public class BatchProcessingThread implements Runnable {
 
     private final List<SinkRecord> records;
-
     private final DefaultAblyBatchClient batchClient;
+    private final ErrantRecordReporter dlqReporter;
+    private final OffsetRegistry offsetRegistryService;
 
-    public BatchProcessingThread(List<SinkRecord> sinkRecords, DefaultAblyBatchClient ablyBatchClient) {
+
+    public BatchProcessingThread(
+        final List<SinkRecord> sinkRecords,
+        final DefaultAblyBatchClient ablyBatchClient,
+        final ErrantRecordReporter dlqReporter,
+        final OffsetRegistry offsetRegistryService) {
         this.records = sinkRecords;
         this.batchClient = ablyBatchClient;
+        this.dlqReporter = dlqReporter;
+        this.offsetRegistryService = offsetRegistryService;
     }
     @Override
     public void run() {
-        batchClient.publishBatch(records);
+        batchClient.publishBatch(records, this.dlqReporter, offsetRegistryService);
     }
 }
